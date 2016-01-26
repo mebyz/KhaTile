6fe6e8754d1df17b38ea961a90903d75e0ae12d2
Emmanuel BOTROS YOUSSEF <emmanuel.botros@gmail.com>
2016-01-26 01:50:58 +0100

    work on terrain layers

--- src/Shaders/simple.frag.glsl
+++ src/Shaders/simple.frag.glsl
@@ -8,12 +8,16 @@ varying vec2 vUV;
 varying float vHeight;
 
 // Values that stay constant for the whole mesh.
+uniform sampler2D sand;
 uniform sampler2D stone;
 uniform sampler2D grass;
 
 void kore() {
 
   // Output color = color of the texture at the specified UV
-  gl_FragColor = texture2D(grass, vUV)*vHeight/100.0+texture2D(stone, vUV)*(1.0-vHeight/50.0);
+  vec4 sd = (smoothstep(-100.0, 0.0, vHeight) - smoothstep(15.0, 25.0, vHeight)) * texture2D( sand, vUV );
+  vec4 s = (smoothstep(0.18, 32.0, vHeight) - smoothstep(30.0, 40.0, vHeight)) * texture2D( stone, vUV );
+  vec4 g = (smoothstep(25.0, 50.0, vHeight) - smoothstep(95.0, 99.0, vHeight)) * texture2D( grass, vUV );
+  gl_FragColor = g+s+sd;
 }
     
