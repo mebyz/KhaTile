2fb69dcc99a77977909bd799160ddf37486bdde1
mebyz <emmanuel.botros@gmail.com>
2023-01-19 21:19:00 +0400

    format

--- Sources/Main.hx
+++ Sources/Main.hx
@@ -5,17 +5,15 @@ import kha.System;
 
 class Main {
 	public static function main() {
-	
-
-	 	
-		System.start({title:"PlaneInstance", width:800, height:600},  function (_) {
-			
+		System.start({title: "PlaneInstance", width: 800, height: 600}, function(_) {
 			var game = new PlaneInstance();
 
-            Scheduler.addTimeTask(function () { game.update(); }, 0, 1 / 50);
-            System.notifyOnFrames(function (f) { game.render(f); });   
-
-    	});
+			Scheduler.addTimeTask(function() {
+				game.update();
+			}, 0, 1 / 50);
+			System.notifyOnFrames(function(f) {
+				game.render(f);
+			});
+		});
 	}
-	
 }
--- Sources/PlaneInstance.hx
+++ Sources/PlaneInstance.hx
@@ -10,13 +10,11 @@ import kha.math.FastVector3;
 import noisetile.NoiseTile;
 import primitive.PlaneModel;
 import primitive.TerrainModel;
-
 import instances.Instances;
 
 class PlaneInstance {
-	
-	var planes : Array<TerrainModel>;
-	var planes2 : Array<PlaneModel>;
+	var planes:Array<TerrainModel>;
+	var planes2:Array<PlaneModel>;
 	var mvp:FastMatrix4;
 
 	var model:FastMatrix4;
@@ -24,9 +22,10 @@ class PlaneInstance {
 	var projection:FastMatrix4;
 
 	public var moveForward = false;
-    public var moveBackward = false;
-    public var strafeLeft = false;
-    public var strafeRight = false;
+	public var moveBackward = false;
+	public var strafeLeft = false;
+	public var strafeRight = false;
+
 	var isMouseDown = false;
 	var mouseX = 0.0;
 	var mouseY = 0.0;
@@ -36,104 +35,97 @@ class PlaneInstance {
 	var speed = 2000.0; // 3 units / second
 	var mouseSpeed = 0.005;
 
-    var lastTime = 0.0;
-    var position:FastVector3 = new FastVector3(0, 100, 5); // Initial position: on +Z
+	var lastTime = 0.0;
+	var position:FastVector3 = new FastVector3(0, 100, 5); // Initial position: on +Z
 	var horizontalAngle = 3.14; // Initial horizontal angle: toward -Z
 	var verticalAngle = 0.0; // Initial vertical angle: none
-	var instancesCollection : Instances;
+	var instancesCollection:Instances;
 
-   	var lastPosition : FastVector3;
+	var lastPosition:FastVector3;
 
 	var gridSize = 30;
-	var tilePx =15;
-	var tileSize =200;
+	var tilePx = 15;
+	var tileSize = 200;
 
 	public function new() {
 		Assets.loadEverything(loadingFinished);
 	}
+
 	public function loadingFinished() {
-			
-		var nt : Dynamic= new NoiseTile(gridSize,gridSize,tilePx);
+		var nt:Dynamic = new NoiseTile(gridSize, gridSize, tilePx);
 
 		planes = new Array();
 		planes2 = new Array();
 
 		for (j in 0...gridSize)
 			for (i in 0...gridSize)
-				planes.push(new TerrainModel(nt.t.tiles[i+j*gridSize],i,j,{ w:tileSize, h:tileSize, x:tilePx, y:tilePx }));
+				planes.push(new TerrainModel(nt.t.tiles[i + j * gridSize], i, j, {
+					w: tileSize,
+					h: tileSize,
+					x: tilePx,
+					y: tilePx
+				}));
 
-		//water
-		//planes2.push(new PlaneModel(0,0,{ w:500, h:500, x:10, y:10 }));
-
+		// water
+		// planes2.push(new PlaneModel(0,0,{ w:500, h:500, x:10, y:10 }));
 
 		projection = FastMatrix4.perspectiveProjection(45.0, 4.0 / 3.0, 0.1, 100000.0);
-		
+
 		view = FastMatrix4.lookAt(new FastVector3(7, 0, 7), // Camera at (4, 3, 3)
-								  new FastVector3(0, 0, 0), //  look at origin
-								  new FastVector3(0, 1, 0) // Head is up, set (0, -1, 0) to look upside down
+			new FastVector3(0, 0, 0), //  look at origin
+			new FastVector3(0, 1, 0) // Head is up, set (0, -1, 0) to look upside down
 		);
-		
+
 		model = FastMatrix4.identity();
 		mvp = FastMatrix4.identity();
 		mvp = mvp.multmat(projection);
 		mvp = mvp.multmat(view);
 		mvp = mvp.multmat(model);
 
-		//instancesCollection = new Instances('grass',2,2,model,view,projection,mvp);
-		
+		// instancesCollection = new Instances('grass',2,2,model,view,projection,mvp);
+
 		// Add mouse and keyboard listeners
 		kha.input.Mouse.get().notify(onMouseDown, onMouseUp, onMouseMove, null);
 		kha.input.Keyboard.get().notify(onKeyDown, onKeyUp);
 
 		// Used to calculate delta time
 		lastTime = Scheduler.time();
+	}
 
-    }
+	public function update() {
+		if (position != lastPosition) {
+			var h = 500; // NoiseTile.getHeight(Std.int(position.z/tileSize*tilePx),Std.int(position.x/tileSize*tilePx));
 
-
-    public function update() {
-    	if (position != lastPosition) {
-			var h = 500;// NoiseTile.getHeight(Std.int(position.z/tileSize*tilePx),Std.int(position.x/tileSize*tilePx));
-			
-			//if (h < -200) 
+			// if (h < -200)
 			//	h=-200;
-			//h=500;
-			
-			//DISABLING stick to ground for now
-			//if (h<-300) h= -300;	
-			position.y=h;
-			//trace(position);
+			// h=500;
 
+			// DISABLING stick to ground for now
+			// if (h<-300) h= -300;
+			position.y = h;
+			// trace(position);
 		}
 		lastPosition = position;
-		
 
-    	/*if (instancesCollection != null)
-	    	instancesCollection.updateAll();*/
-    	// Compute time difference between current and last frame
+		/*if (instancesCollection != null)
+			instancesCollection.updateAll(); */
+		// Compute time difference between current and last frame
 		var deltaTime = Scheduler.time() - lastTime;
 		lastTime = Scheduler.time();
 
 		// Compute new orientation
-		//if (isMouseDown) {
-			horizontalAngle += mouseSpeed * mouseDeltaX * -1;
-			verticalAngle += mouseSpeed * mouseDeltaY * -1;
-		//}
+		// if (isMouseDown) {
+		horizontalAngle += mouseSpeed * mouseDeltaX * -1;
+		verticalAngle += mouseSpeed * mouseDeltaY * -1;
+		// }
 
 		// Direction : Spherical coordinates to Cartesian coordinates conversion
-		var direction = new FastVector3(
-			Math.cos(verticalAngle) * Math.sin(horizontalAngle),
-			Math.sin(verticalAngle),
-			Math.cos(verticalAngle) * Math.cos(horizontalAngle)
-		);
-		
+		var direction = new FastVector3(Math.cos(verticalAngle) * Math.sin(horizontalAngle), Math.sin(verticalAngle),
+			Math.cos(verticalAngle) * Math.cos(horizontalAngle));
+
 		// Right vector
-		var right = new FastVector3(
-			Math.sin(horizontalAngle - 3.14 / 2.0), 
-			0,
-			Math.cos(horizontalAngle - 3.14 / 2.0)
-		);
-		
+		var right = new FastVector3(Math.sin(horizontalAngle - 3.14 / 2.0), 0, Math.cos(horizontalAngle - 3.14 / 2.0));
+
 		// Up vector
 		var up = right.cross(direction);
 
@@ -157,85 +149,87 @@ class PlaneInstance {
 
 		// Look vector
 		var look = position.add(direction);
-		
-
 
 		// Camera matrix
 		view = FastMatrix4.lookAt(position, // Camera is here
-							  look, // and looks here : at the same position, plus "direction"
-							  up // Head is up (set to (0, -1, 0) to look upside-down)
+			look, // and looks here : at the same position, plus "direction"
+			up // Head is up (set to (0, -1, 0) to look upside-down)
 		);
-
 
 		// Update model-view-projection matrix
 		mvp = FastMatrix4.identity();
-		if (projection !=null)
+		if (projection != null)
 			mvp = mvp.multmat(projection);
-		if (view !=null)
+		if (view != null)
 			mvp = mvp.multmat(view);
-		if (model !=null)
+		if (model != null)
 			mvp = mvp.multmat(model);
 
 		mouseDeltaX = 0;
 		mouseDeltaY = 0;
+	}
 
-    }
+	function onMouseDown(button:Int, x:Int, y:Int) {
+		isMouseDown = true;
+	}
 
-    function onMouseDown(button:Int, x:Int, y:Int) {
-    	isMouseDown = true;
-    }
+	function onMouseUp(button:Int, x:Int, y:Int) {
+		isMouseDown = false;
+	}
 
-    function onMouseUp(button:Int, x:Int, y:Int) {
-    	isMouseDown = false;
-    }
+	function onMouseMove(x:Int, y:Int, movementX:Int, movementY:Int) {
+		mouseDeltaX = x - mouseX;
+		mouseDeltaY = y - mouseY;
 
-    function onMouseMove(x:Int, y:Int, movementX:Int, movementY:Int) {
-    	mouseDeltaX = x - mouseX;
-    	mouseDeltaY = y - mouseY;
-
-    	mouseX = x;
-    	mouseY = y;
-    }
+		mouseX = x;
+		mouseY = y;
+	}
 
 	// Create callback functions for when keys are pressed
-	public function onKeyDown(key: kha.input.KeyCode):Void {
+	public function onKeyDown(key:kha.input.KeyCode):Void {
 		trace(key + " down");
 
-        if (key == KeyCode.Up) this.moveForward = true;
-        else if (key == KeyCode.Down) moveBackward = true;
-        else if (key == KeyCode.Left) strafeLeft = true;
-        else if (key == KeyCode.Right) strafeRight = true;
-	  }
-	  public function onKeyUp(key: kha.input.KeyCode):Void {
+		if (key == KeyCode.Up)
+			this.moveForward = true;
+		else if (key == KeyCode.Down)
+			moveBackward = true;
+		else if (key == KeyCode.Left)
+			strafeLeft = true;
+		else if (key == KeyCode.Right)
+			strafeRight = true;
+	}
+
+	public function onKeyUp(key:kha.input.KeyCode):Void {
 		trace(key + " up");
 
-		if (key == KeyCode.Up) moveForward = false;
-        else if (key == KeyCode.Down) moveBackward = false;
-        else if (key == KeyCode.Left) strafeLeft = false;
-        else if (key == KeyCode.Right) strafeRight = false;
-	  }
+		if (key == KeyCode.Up)
+			moveForward = false;
+		else if (key == KeyCode.Down)
+			moveBackward = false;
+		else if (key == KeyCode.Left)
+			strafeLeft = false;
+		else if (key == KeyCode.Right)
+			strafeRight = false;
+	}
 
 	public function render(frames:Array<Framebuffer>) {
-
-
 		var frame = frames[0];
 		var g = frame.g4;
-	    g.begin();
+		g.begin();
 		g.clear(Color.Black);
 
-		if (planes!=null)
+		if (planes != null)
 			for (plane in planes)
-				plane.drawPlane(frame,mvp);
-			
+				plane.drawPlane(frame, mvp);
 
 		/*if (planes2!=null)
-			for (plane in planes2)
-				plane.drawPlane(frame,mvp);
+				for (plane in planes2)
+					plane.drawPlane(frame,mvp);
 
-		if (instancesCollection != null) {
-			instancesCollection.render(frame,model,view,projection);
+			if (instancesCollection != null) {
+				instancesCollection.render(frame,model,view,projection);
 		}*/
 
 		g.end();
-    }
+	}
 }
