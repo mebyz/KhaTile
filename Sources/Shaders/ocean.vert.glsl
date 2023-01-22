d5e02eeb3b343b94e95acd03457b8a1a2e0d8127
mebyz <emmanuel.botros@gmail.com>
2023-01-22 14:51:56 +0400

    clean fog code in water shader

--- Sources/Shaders/ocean.vert.glsl
+++ Sources/Shaders/ocean.vert.glsl
@@ -4,28 +4,24 @@ precision highp float;
 uniform mat4 MVP;
 uniform sampler2D s_texture;
 
+in vec2 uv;
+in vec3 in_normal;
 in vec4 a_position;
-/*layout(location = 1) in vec3 in_position;
-layout(location = 2) in vec3 in_normal;*/
-out vec2 v_textureCoordinates;
-layout(location = 1) in vec2 uv;
-layout(location = 2) in vec3 in_normal;
 
+uniform float time;
 uniform mat4 model_matrix, view_matrix, projection_matrix;
 
+out vec2 v_textureCoordinates;
 out vec3 world_pos;
 out vec3 world_normal;
-uniform float time;
+out vec4 viewSpace;
 
 const float pi = 3.14285714286;
 
-out vec4 viewSpace;
-
 void main() {
     vec4 vertexCoord = a_position;
-    /*float distance = length(vertexCoord);
-    //vertexCoord.y = 1.0;
-    vertexCoord.y += sin(3.0 * pi * distance * 0.3 + time) * 2.0;*/
+    float distance = length(vertexCoord);
+    vertexCoord.y += sin(3.0 * pi * distance * 0.3 + time) * 2.0;
     v_textureCoordinates = uv;
     
     //used for lighting models
@@ -34,9 +30,6 @@ void main() {
 
     //send it to fragment shader
     viewSpace = view_matrix * model_matrix * vertexCoord;
-    //gl_Position = projection_matrix * viewSpace;
-    //gl_Position = MVP * vertexCoord;
-    
 
     gl_Position = MVP * vertexCoord;
 }
