bb86dc825b0dc6b6e47e15377fda3cf52ba69586
mebyz <emmanuel.botros@gmail.com>
2023-02-01 11:21:40 +0400

    update

--- khafile.js
+++ khafile.js
@@ -8,4 +8,5 @@ project.addShaders('Sources/Shaders/**');
 project.addAssets('Assets/**');
 project.addAssets('Libraries/instances/Sources/Assets/**');
 await project.addProject("Subprojects/Koui");
+await project.addProject("Subprojects/aura");
 resolve(project);
