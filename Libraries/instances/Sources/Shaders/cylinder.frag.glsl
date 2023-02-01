2387ee32a77bc29f76c3af5196b6d310792f7187
mebyz <emmanuel.botros@gmail.com>
2023-02-01 10:41:16 +0400

    prepare clean

--- Libraries/instances/Sources/Shaders/cylinder.frag.glsl
+++
@@ -1,8 +0,0 @@
-#version 320 es
-precision mediump float;
-
-//out vec3 fragmentColor;
-
-void main() {
-	/*gl_FragColor = vec4(fragmentColor, 1.0);*/
-}
--- Subprojects/Koui/Sources/koui/elements/import.hx
+++
@@ -1,6 +0,0 @@
-package koui.elements;
-
-import koui.events.Event;
-import koui.graphics.KGraphics;
-import koui.utils.Log;
-import koui.theme.Style;
