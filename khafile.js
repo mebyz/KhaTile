554208110e986c0ed5990daa3a84975dc5283fb6
mebyz <emmanuel.botros@gmail.com>
2023-02-08 04:05:45 +0400

    typo

--- khafile.js
+++ khafile.js
@@ -8,6 +8,6 @@ project.addLibrary('colyseus');
 project.addShaders('Sources/Shaders/**');
 project.addAssets('Assets/**');
 project.addAssets('Libraries/instances/Sources/Assets/**');
-await project.addProject("Subprojects/Koui");
+await project.addProject("Subprojects/koui");
 await project.addProject("Subprojects/aura");
 resolve(project);
